076e63f997b5c5d318b91b7f70e72e607877da82
sjpark2710 <98326939+sjpark2710@users.noreply.github.com>
2022-03-16 20:38:29 +0900

    Add files via upload
    innit2

---
+++ 20220316/src/StriingInfo.java
@@ -0,0 +1,17 @@
+
+public class StriingInfo {
+	
+	public static void main(String[]args) {
+		String snum = "20123458";
+		String name = new String("김경석");
+		String dept = new String(new char[] {'컴', '퓨', '터', '공', '학', '과'});
+		String java = new String("java");
+		
+		
+		
+		
+		
+		
+	}
+
+}
